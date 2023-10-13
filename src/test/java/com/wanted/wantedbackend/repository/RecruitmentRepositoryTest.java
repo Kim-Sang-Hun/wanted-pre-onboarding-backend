b50693886e775227d00662ce34aed2aa985d9d04
Sanghun77 <tpspfkdl@naver.com>
2023-10-13 17:27:57 +0900

    refactor: autoconfigure 삭제

--- src/test/java/com/wanted/wantedbackend/repository/RecruitmentRepositoryTest.java
+++ src/test/java/com/wanted/wantedbackend/repository/RecruitmentRepositoryTest.java
@@ -8,12 +8,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = Replace.AUTO_CONFIGURED)
 public class RecruitmentRepositoryTest {
 
   @Autowired
