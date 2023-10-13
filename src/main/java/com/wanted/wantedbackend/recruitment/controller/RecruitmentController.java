b81161a2ae9441d56c67c221b21fad38c9a94708
Sanghun77 <tpspfkdl@naver.com>
2023-10-13 17:22:21 +0900

    refactor: 추가와 수정이 Recruitment 반환하도록 수정, 삭제가 id 반환하도록 수정

--- src/main/java/com/wanted/wantedbackend/recruitment/controller/RecruitmentController.java
+++ src/main/java/com/wanted/wantedbackend/recruitment/controller/RecruitmentController.java
@@ -1,5 +1,6 @@
 package com.wanted.wantedbackend.recruitment.controller;
 
+import com.wanted.wantedbackend.recruitment.model.Recruitment;
 import com.wanted.wantedbackend.recruitment.model.dto.RecruitmentSubmitDto;
 import com.wanted.wantedbackend.recruitment.service.RecruitmentService;
 import javax.validation.Valid;
@@ -23,8 +24,8 @@ public class RecruitmentController {
       @RequestParam(value = "companyId") Long companyId,
       @Valid @RequestBody RecruitmentSubmitDto request
   ) {
-    recruitmentService.createRecruitment(companyId, request);
-    return ResponseEntity.ok("생성 성공");
+    Recruitment recruitment = recruitmentService.createRecruitment(companyId, request);
+    return ResponseEntity.ok("생성 성공" + recruitment.getId());
   }
 
   @PutMapping("/recruitment/update")
@@ -33,8 +34,8 @@ public class RecruitmentController {
       @RequestParam(value = "recruitmentId") Long recruitmentId,
       @Valid @RequestBody RecruitmentSubmitDto request
   ) {
-    recruitmentService.updateRecruitment(companyId, recruitmentId, request);
-    return ResponseEntity.ok("수정 성공");
+    Recruitment recruitment = recruitmentService.updateRecruitment(companyId, recruitmentId, request);
+    return ResponseEntity.ok("수정 성공" + recruitment.getId());
   }
 
   @DeleteMapping("/recruitment/delete")
@@ -42,8 +43,8 @@ public class RecruitmentController {
       @RequestParam(value = "companyId") Long companyId,
       @RequestParam(value = "recruitmentId") Long recruitmentId
   ) {
-    recruitmentService.deleteRecruitment(companyId, recruitmentId);
-    return ResponseEntity.ok("삭제 성공");
+    Long id = recruitmentService.deleteRecruitment(companyId, recruitmentId);
+    return ResponseEntity.ok("삭제 성공" + id);
   }
 
   @GetMapping("/recruitment/get")
